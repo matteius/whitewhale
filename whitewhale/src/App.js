a6d4720ed1ce658a072cebf9b525093c8bcb9a68
Matt Davis <matteius@gmail.com>
2021-10-24 15:51:47 -0400

    reacto hooks example

--- whitewhale/src/App.js
+++ whitewhale/src/App.js
@@ -1,12 +1,18 @@
+import React from "react";
 import './App.css';
 import Post from './components/Post'
 
 function App() {
-  return (
+    let [responseData, setResponseData] = React.useState('');
+    React.useEffect(() => {
+        setResponseData('hello')
+        console.log(responseData)
+    }, [setResponseData, responseData])
+    return (
     <div className="App">
       <Post />
     </div>
-  );
+    );
 }
 
 export default App;
