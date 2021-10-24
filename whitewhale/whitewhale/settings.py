1a4cab1873d7f9f7ee0933d376ce7c83e6906ee9
Matt Davis <matteius@gmail.com>
2021-10-24 06:57:40 -0400

    Add build directory where to look for staticfiles.

--- whitewhale/whitewhale/settings.py
+++ whitewhale/whitewhale/settings.py
@@ -129,6 +129,10 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR / 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "build",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
