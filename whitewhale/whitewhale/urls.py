aca591814ec3aa4fa46abdcb597d29d6812ae225
Matt Davis <matteius@gmail.com>
2021-10-23 20:03:46 -0400

    add basic index view

--- whitewhale/whitewhale/urls.py
+++ whitewhale/whitewhale/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from blog.views import index
+
 urlpatterns = [
+    path('/'), index,
     path('admin/', admin.site.urls),
     path('health/', include('health_check.urls')),
 ]
