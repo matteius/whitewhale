031b13abbd51656b80f9125e9efd0d9a1d6383a1
Matt Davis <matteius@gmail.com>
2021-10-24 18:35:42 -0400

    Adding in call to get posts.

--- whitewhale/blog/urls.py
+++ whitewhale/blog/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('posts', views.BlogEntryListView.as_view()),
+    path('posts/', views.BlogEntryListView.as_view()),
 ]
--- whitewhale/src/App.js
+++ whitewhale/src/App.js
@@ -1,14 +1,27 @@
 import React from "react";
+import axios from 'axios'
 import './App.css';
 import Post from './components/Post'
+
+console.log('No value for FOO yet:', process.env.NODE_ENV);
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+console.log('Now the value for FOO is:', process.env.NODE_ENV);
+var apiUrl = process.env.NODE_ENV === 'production' ? '' : process.env.REACT_APP_SERVER;
+console.log(apiUrl);
+
 
 function App() {
     let [responseData, setResponseData] = React.useState('');
     axios({
         "method": "GET",
-        "url": "/blog/posts/",
+        "url": apiUrl + '/blog/posts/',
         "headers": {
-            "content-type": "application/json"
+	    "Accept": "application/json",
+            "Content-Type": "application/json"
         }
     })
         .then((response) => {
@@ -19,6 +32,11 @@ function App() {
         })
     return (
     <div className="App">
+      <pre>
+        <code>
+          {responseData && JSON.stringify(responseData, null, 4)}
+        </code>
+      </pre>
       <Post />
     </div>
     );
