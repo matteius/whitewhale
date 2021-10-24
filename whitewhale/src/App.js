fb5bf3447f15ea10094777c84d6d60385a959c22
Matt Davis <matteius@gmail.com>
2021-10-24 18:40:08 -0400

    Do not call repeatedly to get blog posts

--- whitewhale/src/App.js
+++ whitewhale/src/App.js
@@ -6,7 +6,7 @@ import Post from './components/Post'
 console.log('No value for FOO yet:', process.env.NODE_ENV);
 
 if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
+    require('dotenv').config();
 }
 
 console.log('Now the value for FOO is:', process.env.NODE_ENV);
@@ -16,29 +16,37 @@ console.log(apiUrl);
 
 function App() {
     let [responseData, setResponseData] = React.useState('');
-    axios({
-        "method": "GET",
-        "url": apiUrl + '/blog/posts/',
-        "headers": {
-	    "Accept": "application/json",
-            "Content-Type": "application/json"
-        }
-    })
+    const fetchData = React.useCallback(() => {
+        axios({
+            "method": "GET",
+            "url": apiUrl + '/blog/posts/',
+            "headers": {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            }
+        })
         .then((response) => {
             setResponseData(response.data)
         })
         .catch((error) => {
             console.log(error)
         })
+    }, [])
+
+    React.useEffect(() => {
+        fetchData()
+    }, [fetchData])
+
     return (
-    <div className="App">
-      <pre>
+        <div className="App">
+            <button type='button' onClick={fetchData}>Click for Data</button>
+            <pre>
         <code>
           {responseData && JSON.stringify(responseData, null, 4)}
         </code>
       </pre>
-      <Post />
-    </div>
+            <Post/>
+        </div>
     );
 }
 
