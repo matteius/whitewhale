a3fd30e0426d493f1ff2b5e4ba622574d5fbffe8
Matt Davis <matteius@gmail.com>
2021-10-24 15:23:12 -0400

    Working on react pages for blog posts.

---
+++ whitewhale/src/components/Post.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Post = () => {
+    return (
+        <>
+            <section>
+                <h3>Post title will appear here</h3>
+                <p> Post contents will appear here</p>
+                <button>Edit</button>
+                <button>Delete</button>
+            </section>
+        </>
+    )
+}
+export default Post
