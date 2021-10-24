626c803e9f9ac75e9f3a0bf504686acf367e7333
Matt Davis <matteius@gmail.com>
2021-10-24 18:42:43 -0400

    Add admin functionality

--- whitewhale/blog/admin.py
+++ whitewhale/blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import BlogEntry
+
+
+class BlogEntryAdmin(admin.ModelAdmin):
+    list_display = ['slug', 'publish_date', 'title', 'subtitle', 'meta_description', 'body']
+
+
+admin.site.register(BlogEntry, BlogEntryAdmin)
