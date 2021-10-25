cf918cd2b67c071d4085568b3b6f2365bdd799bc
Matt Davis <matteius@gmail.com>
2021-10-24 22:01:02 -0400

    include purecss

--- whitewhale/src/index.js
+++ whitewhale/src/index.js
@@ -1,6 +1,7 @@
+import 'purecss/build/pure.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
