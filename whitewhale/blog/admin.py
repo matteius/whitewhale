f0548a7ea3b360f55e269040b01ee6297722f62b
Matt Davis <matteius@gmail.com>
2021-10-29 23:40:43 -0400

    Add UserProfile to admin

--- whitewhale/blog/admin.py
+++ whitewhale/blog/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 
-from .models import BlogEntry
+from .models import BlogEntry, UserProfile
 
 
 class BlogEntryAdmin(admin.ModelAdmin):
     list_display = ['slug', 'publish_date', 'title', 'subtitle', 'meta_description', 'body']
 
 
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'website']
+
+
 admin.site.register(BlogEntry, BlogEntryAdmin)
+admin.site.register(UserProfile, UserProfileAdmin)
