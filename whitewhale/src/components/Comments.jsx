3aae6b4420b146193f3aa44a593abeb3c6245ba3
Matt Davis <matteius@gmail.com>
2021-10-31 08:28:28 -0400

    formatting

--- whitewhale/src/components/Comments.jsx
+++ whitewhale/src/components/Comments.jsx
@@ -4,19 +4,19 @@ const Comments = (comments) => {
     if (!comments.comments || comments.length === 0) {
         return <div>No comments available for this Post.</div>
     } else {
-        return (<div><h4>Comments</h4>
-		{comments.comments.map(comment => {
-	    let date = new Date(comment.created);
-            let name = comment.name;
-            if (comment.email) {
-                name =  <a href={"mailto:" + comment.email}>{comment.name}</a>;
-            }
-                 return  <div key={comment.id} className="comment">
+        return (<div><h4>Approved Comments</h4>
+            {comments.comments.map(comment => {
+                    let date = new Date(comment.created);
+                    let name = comment.name;
+                    if (comment.email) {
+                        name = <a href={"mailto:" + comment.email}>{comment.name}</a>;
+                    }
+                    return <div key={comment.id} className="comment">
                         <div className="row">{comment.response}</div>
                         <div className="row">By: {name} on {date.toLocaleString()}</div>
                     </div>
-            }
-        )}
+                }
+            )}
         </div>)
     }
 }
