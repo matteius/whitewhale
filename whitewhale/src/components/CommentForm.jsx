a64eb0922af7c5eef44eef212dcb1ee723d766f1
Matt Davis <matteius@gmail.com>
2021-10-31 04:36:22 -0400

    Adding csrf token to the comments form.

--- whitewhale/src/components/CommentForm.jsx
+++ whitewhale/src/components/CommentForm.jsx
@@ -1,5 +1,7 @@
 import axios from 'axios'
 import {Formik, Field, Form} from "formik";
+import getCookie from "../utils"
+
 
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
@@ -9,16 +11,25 @@ console.log('Now the value for NODE_ENV is:', process.env.NODE_ENV);
 var apiUrl = process.env.NODE_ENV === 'production' ? '' : process.env.REACT_APP_SERVER;
 
 
+var csrftoken = getCookie('csrftoken');
+
+
 const CommentForm = (entry_id) => {
     console.log(entry_id);
     return (
         <div>
             <h4>Add New Comment</h4>
             <Formik
-                initialValues={{entry: entry_id.entry_id, name: "", email: "", response: ""}}
+                initialValues={{csrfmiddlewaretoken: csrftoken, entry: entry_id.entry_id,
+                    email: "", name: "", response: ""}}
                 onSubmit={async (values) => {
                     await new Promise((resolve) => setTimeout(resolve, 500));
                     alert(JSON.stringify(values, null, 2));
+                    headers = {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json',
+                        'X-CSRFToken': csrftoken
+                    }
                     axios.post( apiUrl + '/blog/comment/', values)
                         .then(response => { console.log('The response was: ' + response) })
                         .catch(error => {
