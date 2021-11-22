9359dd7e4814c2c78d084703c1ffc337bc11ae35
Matt Davis <matteius@gmail.com>
2021-11-22 02:21:34 -0500

    Introduce react-router and react-router-dom and convert existing nav to use it.

--- whitewhale/src/App.js
+++ whitewhale/src/App.js
@@ -1,10 +1,27 @@
 import React, {Component} from "react";
+import { Routes, Route, Link } from "react-router-dom";
+
 import './App.css';
-
-
 import Blog from "./components/Blog";
 import './css/Blog.css';
 
+
+function About() {
+  return (
+    <>
+      <main>
+        <h2>Who are we?</h2>
+        <p>
+          That feels like an existential question, don't you
+          think?
+        </p>
+      </main>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+    </>
+  );
+}
 
 
 class App extends Component {
@@ -18,13 +35,17 @@ class App extends Component {
                 <div className="blog-masthead">
                     <div className="container">
                         <nav className="blog-nav">
-                            <a className="blog-nav-item active" href="#">Home</a>
-                            <a className="blog-nav-item" href="#">App2</a>
-                            <a className="blog-nav-item" href="#">About</a>
+                            <Link className="blog-nav-item" to="/">Blog</Link>
+                            <Link className="blog-nav-item" to="/app2">App2</Link>
+                            <Link className="blog-nav-item" to="/about">About</Link>
                         </nav>
                     </div>
                 </div>
-                <Blog />
+                <Routes>
+                    <Route path="/" element={<Blog />}/>
+                    <Route path="/app2" element={<Blog />}/>
+                    <Route path="/about" element={<About />}/>
+                </Routes>
             </>
         );
     }
