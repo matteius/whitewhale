92a90bb6c5cedaba663182af489373c765e34f35
Matt Davis <matteius@gmail.com>
2021-11-24 03:13:55 -0500

    Add missing About component (got wiped out somehow).

---
+++ whitewhale/src/components/About.jsx
@@ -0,0 +1,20 @@
+import React, {Component} from "react";
+
+
+class About extends Component {
+    render() {
+        return (
+            <>
+                <main>
+                    <h2>Who are we?</h2>
+                    <p>
+                        That feels like an existential question, don't you
+                        think?
+                    </p>
+                </main>
+            </>
+        );
+    }
+}
+
+export default About;
